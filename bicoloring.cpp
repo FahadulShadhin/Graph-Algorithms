4cc0581d4624f73dd56c415db30722dcee673523
Fahadul Islam Shadhin <fahadulshadhin17@gmail.com>
2020-02-17 10:47:30 -0600

    Update bicoloring.cpp

--- bicoloring.cpp
+++ bicoloring.cpp
@@ -59,7 +59,7 @@ int main() {
     for(int i=0; i<edges; i++) {
         cin >> u >> v;
         graph[u].push_back(v);
-        graph[v].push_back(u);
+        graph[v].push_back(u); // Undirected graph.
     }
 
     memset(color, WHITE, sizeof color); // Initially color of all nodes is set to white
