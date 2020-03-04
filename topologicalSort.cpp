fff1b7a78eb32d7b5e421afb20c17543b88a9f11
Fahadul Islam Shadhin <fahadulshadhin17@gmail.com>
2020-03-04 02:11:05 -0600

    Add files via upload

---
+++ topologicalSort.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+#define MAX 1001
+using namespace std;
+
+int nodes, edges, u, v;
+vector<int> graph[MAX];
+bool visited[MAX];
+int in_degree[MAX];
+
+void topologicalSort() {
+    memset(visited, false, sizeof visited);
+    vector<int> T;
+    queue<int> q;
+
+    for(int i=1; i<=nodes; i++) {
+        if(in_degree[i] == 0) {
+            q.push(i);
+            visited[i] = true;
+        }
+    }
+
+    while(!q.empty()) {
+        int cur = q.front();
+        q.pop();
+        T.push_back(cur);
+        for(int i=0; i<graph[cur].size(); i++) {
+            int next = graph[cur][i];
+            if(visited[next] == false) {
+                in_degree[next] = in_degree[next] - 1;
+                if(in_degree[next] == 0) {
+                    q.push(next);
+                    visited[next] = true;
+                }
+            }
+        }
+    }
+
+    printf("Topological sort: \n");
+    for(int i=0; i<T.size(); i++) 
+        printf("%d ", T[i]);
+    printf("\n");
+}
+
+int main() {
+    cin >> nodes >> edges;
+    for(int i=0; i<edges; i++) {
+        cin >> u >> v;
+        graph[u].push_back(v);
+        //graph[v].push_back(u); // directed graph.
+        in_degree[v]++;
+    }
+
+    topologicalSort();
+    return 0;
+}
